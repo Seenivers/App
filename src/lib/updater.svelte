e6ca16b554a96b061754de3676c202ccdd974c0b
Blacky <ralfretter2004@gmail.com>
2024-01-18 18:46:11 +0100

    added updater

---
+++ src/lib/updater.svelte
@@ -0,0 +1,63 @@
+<script async script lang="ts">
+	import {
+		checkUpdate,
+		installUpdate,
+		onUpdaterEvent,
+		type UpdateResult
+	} from '@tauri-apps/api/updater';
+	import { relaunch } from '@tauri-apps/api/process';
+	import { onMount } from 'svelte';
+
+	let update: boolean = false;
+	let data: UpdateResult;
+
+	onMount(async () => {
+		data = await checkUpdate();
+		console.log(data);
+
+		if (data.shouldUpdate) {
+			update = true;
+		}
+	});
+
+	async function updateInstall() {
+		const unlisten = await onUpdaterEvent(({ error, status }) => {
+			console.log('Updater event', error, status);
+		});
+
+		try {
+			await installUpdate();
+			await relaunch();
+		} catch (error) {
+			console.error(error);
+		}
+
+		unlisten();
+	}
+</script>
+
+{#if update && data}
+	<div
+		class="fixed bottom-5 right-5 h-fit w-80 rounded-md border border-neutral bg-neutral-content text-black"
+	>
+		<span class="mx-3 text-lg">Eine neue Version ist Verfügbar</span>
+		<div class="p-3">
+			<div class="mb-3 grid">
+				<span>Version: {data.manifest?.version}</span>
+				<span>Hinweise zur Veröffentlichung: <br />{data.manifest?.body}</span>
+			</div>
+			<div>
+				<span>Updatet Herunterladen</span>
+				<div class="flex gap-2 w-fit">
+					<button class="btn w-full" on:click={updateInstall}>JA</button>
+					<button
+						class="btn w-full"
+						on:click={() => {
+							update = false;
+						}}>NEIN</button
+					>
+				</div>
+			</div>
+		</div>
+	</div>
+{/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script>
+	import Updater from '$lib/updater.svelte';
 	import '../app.postcss';
 	import { load } from '../ts/dir';
 
 	load();
-	
-	// get(mediaLibrary)
 </script>
 
 <div class="min-h-screen w-full bg-base-300 text-base-content h-fit">
+	<Updater/>
 	<slot />
 </div>
