fbcfe4585adb2d26bc5843a1feba72ad31a9247f
Blacky <66371975+BlackTiger007@users.noreply.github.com>
2024-08-11 00:10:39 +0200

    init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,5 @@
 <script>
-	import Updater from '$lib/updater.svelte';
-	import '../app.postcss';
-	import { load } from '../ts/dir';
-
-	load();
+	import '../app.css';
 </script>
 
-<div class="min-h-screen w-full bg-base-300 text-base-content h-fit">
-	<Updater/>
-	<slot />
-</div>
+<slot></slot>
--- src/ts/db.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from "svelte/store";
-
-export let mediaLibrary = writable<Medias>()
-
-export let settings = writable<Settings>()
