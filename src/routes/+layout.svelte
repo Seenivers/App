7565415a034f94ce3d4f7837c367ba7c2bbb664f
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-10-03 17:39:14 +0200

    fix(layout): implement cleanup logic in onDestroy lifecycle method

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
+	import { onDestroy } from 'svelte';
 	import '../app.css';
+	import { cleanupData } from '$lib/db';
+
+	onDestroy(() => {
+		cleanupData();
+	});
 </script>
 
 <div class="flex h-fit min-h-screen flex-col bg-base-300">
