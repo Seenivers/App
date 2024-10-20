225e751446e34f902f2a38efe374ba8ac6eece1d
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-10-20 20:57:31 +0200

    feat(release): integrate semantic-release with configuration for main and next branches

---
+++ release.config.cjs
@@ -0,0 +1,26 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+	branches: [
+		'main',
+		{
+			name: 'next',
+			prerelease: true,
+			channel: 'next'
+		}
+	],
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/github',
+		'@semantic-release/changelog',
+		[
+			'@semantic-release/git',
+			{
+				assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+				message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+			}
+		]
+	]
+};
