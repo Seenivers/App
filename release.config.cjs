ced767e6764c2a9bed8f849c1855222969b41071
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-10-25 17:06:20 +0200

    chore(release): add @semantic-release/npm to update package version

--- release.config.cjs
+++ release.config.cjs
@@ -13,6 +13,7 @@ module.exports = {
 	plugins: [
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
+		'@semantic-release/npm',
 		'@semantic-release/github',
 		'@semantic-release/changelog',
 		[
