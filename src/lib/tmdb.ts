da73d7ce61bacbe2a00f38d592d0a12499788c0b
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-11-04 11:32:15 +0100

    refactor(api): optimize language retrieval and simplify initialization of tmdb instance

--- src/lib/tmdb.ts
+++ src/lib/tmdb.ts
@@ -2,14 +2,11 @@ import { fetch } from '@tauri-apps/plugin-http';
 import TMDB from '@blacktiger/tmdb';
 import { data } from './db';
 import { newToast } from './toast/toast';
+import { get } from 'svelte/store';
 
 const STATIC_KEY = '51baf525-2720-4c43-8d34-b759bb71ae88';
 let apiKey: string | null = null; // Initialisiere apiKey als null
-const language = () => {
-	let lang = window.navigator.language; // Setze standardmäßige Sprache
-	data.subscribe((data) => (lang = data.settings.language));
-	return lang;
-};
+const language = data ? get(data).settings.language : window.navigator.language.split('-')[0]; // Setze standardmäßige Sprache
 
 async function newApiKey() {
 	const response = await fetch('https://seenivers.com/api/api-key', {
@@ -35,7 +32,7 @@ let tmdb: TMDB;
 // Stelle sicher, dass die Funktion aufgerufen wird
 newApiKey().then((success) => {
 	if (success && apiKey) {
-		tmdb = new TMDB(apiKey, language()); // Initialisiere tmdb erst, wenn apiKey gesetzt ist
+		tmdb = new TMDB(apiKey, language); // Initialisiere tmdb erst, wenn apiKey gesetzt ist
 	} else {
 		console.error('API-Key konnte nicht abgerufen werden.');
 		newToast('error', 'API-Key konnte nicht abgerufen werden.');
