794a0ed7b85081631226d9fdb1890d90c346f4d6
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-11-11 20:07:05 +0100

    fix(db): set `path` as unique in `movies` schema to prevent duplicate entries
    - Updated `path` column in `movies` table to be unique.
    - Ensures each movie file path is stored only once in the database.

--- src/lib/db/schema/movies.ts
+++ src/lib/db/schema/movies.ts
@@ -3,7 +3,7 @@ import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const movies = sqliteTable('movies', {
 	id: int('id').notNull().primaryKey(),
-	path: text('path').notNull(),
+	path: text('path').notNull().unique(),
 	watched: int('watched', { mode: 'boolean' }).notNull().default(false),
 	watchTime: int('watchTime').notNull().default(0),
 	tmdb: text('tmdb', { mode: 'json' }).$type<Movie>().notNull()
