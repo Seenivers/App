1200c4cedecae6bb0632aa417d5204c0bd08ed77
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-11-18 09:31:20 +0100

    refactor(api): adjust movie search to use Seenivers API endpoint and update types

--- src/lib/add/index.ts
+++ src/lib/add/index.ts
@@ -1,6 +1,8 @@
-import { getMovie, tmdb } from '$lib/tmdb';
+import { getMovie } from '$lib/tmdb';
 import { settings } from '$lib/db/funktion';
 import type { Movie } from '$lib/types/movie';
+import { seeniversURL } from '$lib';
+import type { Search, Movie as SearchMovie } from '$lib/types/searchMovie';
 
 export function buttonClass(searchStatus: string) {
 	switch (searchStatus) {
@@ -39,13 +41,21 @@ export function getIcon(searchStatus: string) {
 /**
  * Suche Filme in der TMDB basierend auf einem Namen und optionalen Parametern.
  */
-export async function searchMovies(name: string, primaryReleaseYear?: string | number) {
-	return tmdb.search.movie(name, {
-		language: settings.language,
-		includeAdult: settings.adult,
-		primaryReleaseYear: primaryReleaseYear?.toString(),
-		page: 1
-	});
+export async function searchMovies(
+	name: string,
+	primaryReleaseYear?: string | number,
+	page = 1
+): Promise<Search<SearchMovie>> {
+	const url = new URL(seeniversURL + '/api/movie/search');
+	url.searchParams.append('name', name);
+	url.searchParams.append('language', settings.language);
+	url.searchParams.append('includeAdult', settings.adult.toString());
+	url.searchParams.append('primaryReleaseYear', primaryReleaseYear?.toString() || '');
+	url.searchParams.append('page', page.toString());
+
+	const result = await fetch(url.toString());
+
+	return (await result.json()) as Search<SearchMovie>;
 }
 
 /**
