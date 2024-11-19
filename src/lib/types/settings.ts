92ae92edcb347848add74b8ade62bc1d5050b2be
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-11-19 18:37:46 +0100

    refactor(types): move Settings type to separate file for better organization

---
+++ src/lib/types/settings.ts
@@ -0,0 +1,20 @@
+export type Settings = {
+	language: string;
+	online: boolean;
+	keywords: string[];
+	adult: boolean;
+	toastPosition: {
+		horizontal: AlertPositionHorizontally;
+		vertical: AlertPositionVertically;
+	};
+};
+
+export type AlertPositionHorizontally =
+	| 'start' // align horizontally to the left
+	| 'center' // align horizontally to the center
+	| 'end'; // align horizontally to the right (default)
+
+export type AlertPositionVertically =
+	| 'top' // align vertically to top
+	| 'middle' // align vertically to middle
+	| 'bottom'; // align vertically to bottom (default)
