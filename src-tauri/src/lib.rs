6301d3fe458842b5a93e80592c37a92220670979
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-11-22 20:30:26 +0100

    feat(logging): integrate advanced logging with @tauri-apps/plugin-log
    - Enhanced the `run` function by integrating `tauri_plugin_log` with advanced logging configurations:
      - Added local timezone strategy.
      - Set a maximum file size of 10MB for logs.
      - Adjusted target filtering to include Webview and log directory output.
      - Excluded logging from specific targets to reduce noise.
    - Updated `desktop-schema.json` to include `"log:default"` permission for logging support.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,16 +9,24 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_http::init())
-        .setup(|app| {
-            if cfg!(debug_assertions) {
-                app.handle().plugin(
-                    tauri_plugin_log::Builder::default()
-                        .level(log::LevelFilter::Info)
-                        .build(),
-                )?;
-            }
-            Ok(())
-        })
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .timezone_strategy(tauri_plugin_log::TimezoneStrategy::UseLocal)
+                .max_file_size((10 * 1024 * 1024) as u128) // 10MB
+                .clear_targets()
+                .target(tauri_plugin_log::Target::new(
+                    tauri_plugin_log::TargetKind::Webview,
+                ))
+                .target(tauri_plugin_log::Target::new(
+                    tauri_plugin_log::TargetKind::LogDir {
+                        file_name: Some("logs".to_string()),
+                    },
+                ))
+                .filter(|metadata| {
+                    metadata.target() != "tao::platform_impl::platform::event_loop::runner"
+                })
+                .build(),
+        )
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
