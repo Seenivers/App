0e290507600bcdd5d9bcd1d9e975beeb2dd31080
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-12-01 17:59:05 +0100

    fix: avoid unsafe call of `error` type by properly typing config object

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'drizzle-kit';
-export default defineConfig({
+import { defineConfig, type Config } from 'drizzle-kit';
+
+const config: Config = {
 	dialect: 'sqlite',
 	schema: './src/lib/db/schema.ts',
 	out: './src-tauri/migrations',
 	dbCredentials: {
 		url: ':memory:'
 	}
-});
+};
+
+export default defineConfig(config);
