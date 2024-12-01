bddae63bd23cb869e06b959b01367b58bfb02cff
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-12-01 12:06:11 +0100

    refactor(env): remove DEV environment check for seeniversURL

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,4 +2,4 @@ import { NoImage } from './SVG';
 
 export const placeholderURL = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(NoImage)}`;
 export const imageURL = 'https://image.tmdb.org/t/p/original';
-export const seeniversURL = import.meta.env.DEV ? 'http://localhost:5173' : 'https://seenivers.com';
+export const seeniversURL = 'https://seenivers.com';
