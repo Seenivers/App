e817334e3a939167e7ac22a4b9f8733599c0414a
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-12-04 21:27:02 +0100

    feat(api): add `getCollection` function to fetch collection details
    - Implemented a new function `getCollection` to fetch collection details from the API.
    - Improved error handling for both `getMovie` and `getCollection` functions, providing detailed error messages.
    - Ensured consistent handling of response data with type casting to `Movie` and `CollectionDetails`.

--- src/lib/tmdb.ts
+++ src/lib/tmdb.ts
@@ -3,6 +3,7 @@ import { error } from '@tauri-apps/plugin-log';
 import type { Movie } from '$lib/types/movie';
 import { settings } from '$lib/db/funktion';
 import { seeniversURL } from '$lib';
+import type { CollectionDetails } from './types/collection';
 
 export async function getMovie(
 	id: number,
@@ -39,6 +40,45 @@ export async function getMovie(
 			error('Fehler: ' + err.message); // Zeige die detaillierte Fehlermeldung
 		} else {
 			error('Ein unbekannter Fehler ist aufgetreten'); // Fallback, falls der Fehler kein Error-Objekt ist
+		}
+	}
+}
+
+export async function getCollection(
+	id: number,
+	language: string = settings.language || window.navigator.language
+) {
+	try {
+		// Erstelle die URL mit den Query-Parametern id und language
+		const url = new URL('/api/collection', seeniversURL);
+		url.searchParams.set('id', id.toString());
+		url.searchParams.set('language', language);
+
+		// Führe den GET-Request aus
+		const response = await fetch(url.toString(), {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+
+		// Überprüfe, ob die Antwort OK ist
+		if (!response.ok) {
+			const errorMessage = await response.text(); // Hole den Text der Fehlermeldung
+			throw new Error(
+				`Fehler beim Abrufen der Collektion (Status: ${response.status}): ${errorMessage}`
+			);
+		}
+
+		// Versuche, die JSON-Antwort zu parsen
+		const data = await response.json();
+		return data as CollectionDetails; // Gebe das Movie-Objekt zurück
+	} catch (err) {
+		// Fehlerbehandlung mit detaillierterer Ausgabe
+		if (err instanceof Error) {
+			error('Fehler: ' + err.message); // Zeige die detaillierte Fehlermeldung
+		} else {
+			error('Ein unbekannter Fehler ist aufgetreten: ' + err); // Fallback, falls der Fehler kein Error-Objekt ist
 		}
 	}
 }
