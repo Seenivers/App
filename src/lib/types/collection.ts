648613550663d6856a87bfa0d7d3ae3459880801
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-12-04 21:24:07 +0100

    feat(types): add `Collection` and `CollectionDetails` types for TMDB collection data

---
+++ src/lib/types/collection.ts
@@ -0,0 +1,27 @@
+export interface Collection {
+	id: number;
+	name: string;
+	overview: string | null;
+	poster_path: string;
+	backdrop_path: string;
+}
+export interface CollectionDetails extends Collection {
+	parts: Movie[];
+}
+
+export interface Movie {
+	id: number;
+	poster_path: string;
+	adult: boolean;
+	overview: string;
+	release_date: string;
+	genre_ids: number[];
+	original_title: string;
+	original_language: string;
+	title: string;
+	backdrop_path: string;
+	popularity: number;
+	vote_count: number;
+	video: boolean;
+	vote_average: number;
+}
