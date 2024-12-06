16e9b191bb9df22a2bce10c1defd57bc78aa9c5b
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-12-06 11:44:23 +0100

    refactor(toast): improve styling and responsiveness for toast notifications

--- src/lib/toast/toast.svelte
+++ src/lib/toast/toast.svelte
@@ -6,6 +6,7 @@
 	import { fly } from 'svelte/transition';
 	import { cubicOut, cubicIn } from 'svelte/easing';
 
+	// Set default positions, falls keine Einstellungen verfügbar sind
 	const positionHorizontally: AlertPositionHorizontally = settings
 		? settings.toastPosition.horizontal
 		: 'end';
@@ -14,13 +15,13 @@
 		: 'bottom';
 </script>
 
-<!-- Toast messages rendering with optimized animation -->
+<!-- Toast Container -->
 <div
-	class="toast z-50 toast-{positionHorizontally} toast-{positionVertically} max-h-96 overflow-y-auto"
+	class={`toast z-50 toast-${positionHorizontally} toast-${positionVertically} max-h-[33vh] min-w-36 max-w-[33vw] overflow-y-auto`}
 >
 	{#each $messages as { type, text }}
 		<div
-			class="alert alert-{type} mb-2 h-full min-h-fit w-full max-w-prose overflow-y-auto rounded-lg p-4 shadow-lg transition-all"
+			class="alert alert-{type} mb-2 min-h-fit w-full max-w-full overflow-y-auto rounded-lg p-4 shadow-lg transition-all"
 			in:fly={{ duration: 400, y: -50, easing: cubicOut }}
 			out:fly={{ duration: 400, y: 100, easing: cubicIn }}
 		>
