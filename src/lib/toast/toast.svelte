25ec1e5aada0f22785afe90c10a9d47c98ec4d35
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-12-10 19:35:07 +0100

    feat(toast): enhance toast container with collapse info panel and custom styling
    - Add collapse info panel for messages with fallback text
    - Modify toast container with custom width and min-height styles

--- src/lib/toast/toast.svelte
+++ src/lib/toast/toast.svelte
@@ -14,15 +14,26 @@
 
 <!-- Toast Container -->
 <div
-	class={`toast z-50 toast-${positionHorizontally} toast-${positionVertically} max-h-[33vh] min-w-[9rem] max-w-[33vw] overflow-y-auto`}
+	class={`toast z-50 toast-${positionHorizontally} toast-${positionVertically} !min-w-unset w-fit max-w-full`}
 >
-	{#each $messages as { type, text }}
+	<details class="collapse bg-base-100">
+		<summary class="collapse-title text-xl font-medium">Info-Panel</summary>
 		<div
-			class="alert alert-{type} mb-2 min-h-fit w-full max-w-full overflow-y-auto rounded-lg p-4 shadow-lg transition-all"
-			in:fly={{ duration: 400, y: -50, easing: cubicOut }}
-			out:fly={{ duration: 400, y: 100, easing: cubicIn }}
+			class="!min-h-unset !min-w-unset collapse-content mb-3 box-border grid max-h-[33vh] w-full flex-col flex-wrap gap-3 overflow-y-auto p-3"
 		>
-			<span>{text}</span>
+			{#if $messages.length > 0}
+				{#each $messages as { type, text }}
+					<div
+						class="alert alert-{type} min-h-fit w-fit flex-shrink-0 snap-center whitespace-normal break-words rounded-lg p-4 shadow-lg transition-all"
+						in:fly={{ duration: 400, y: -50, easing: cubicOut }}
+						out:fly={{ duration: 400, y: 100, easing: cubicIn }}
+					>
+						<span>{text}</span>
+					</div>
+				{/each}
+			{:else}
+				<p class="text-center">Keine Meldungen</p>
+			{/if}
 		</div>
-	{/each}
+	</details>
 </div>
