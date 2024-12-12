e20fdb585bfc76415785e57050fd39ce36d494a4
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-12-12 15:04:17 +0100

    fix(online-status): initialize `isOnline` store with current navigator.onLine status
    - Set initial value of `isOnline` store to `window.navigator.onLine` to reflect the user's current online status.

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -4,4 +4,4 @@ import type { MovieSearchContext } from '$lib/types/add';
 // Erstelle einen `writable` Store für den Status
 export const status = writable<MovieSearchContext[]>([]);
 
-export const isOnline = writable<boolean>(false);
+export const isOnline = writable<boolean>(window.navigator.onLine);
