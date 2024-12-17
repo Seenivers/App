91f3c1f8c39f83a82f2b7b433d543b4c9317d333
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-12-17 18:53:27 +0100

    fix: change 'status' to const as it is never reassigned

--- src/lib/stores.svelte.ts
+++ src/lib/stores.svelte.ts
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 import type { MovieSearchContext } from '$lib/types/add';
 
-export let status = $state<MovieSearchContext[]>([]);
+export const status = $state<MovieSearchContext[]>([]);
 
 export const isOnline = writable<boolean>(window.navigator.onLine);
