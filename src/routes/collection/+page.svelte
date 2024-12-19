cba92d98a257bebce33ce89dc59ee4babf19c157
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-12-19 11:40:58 +0100

    refactor(collection): improve data handling and structure for page load

--- src/routes/collection/+page.svelte
+++ src/routes/collection/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import { getCollection, settings } from '$lib/db/funktion';
+	import { settings } from '$lib/db/funktion';
 	import { image } from '$lib/image/image';
 	import Navbar from '$lib/Navbar.svelte';
 	import Img from '$lib/image/Img.svelte';
@@ -10,106 +10,104 @@
 	}
 
 	let { data }: Props = $props();
-	const id = data.id;
 
 	let isGridView = $state(false); // Startwert für das Layout
 </script>
 
-<Navbar back={true}></Navbar>
+<Navbar back={true} />
 
 <!-- Main -->
 <main class="z-0 flex flex-col items-center p-3 md:p-5">
-	{#await getCollection(id) then collection}
-		{#if collection}
-			<div class="mx-auto w-full max-w-full">
-				{#await image(collection.backdrop_path, 'backdrops', true) then backdropImage}
-					<div
-						class="hero rounded-box bg-base-200"
-						style="background-image: url({backdropImage.src});"
-					>
-						<div class="hero-overlay rounded-box bg-opacity-90"></div>
-						<div class="hero-content flex-col gap-4 lg:flex-row">
-							<Img
-								alt="Poster"
-								params={[collection.poster_path, 'backdrops', true]}
-								class="max-w-xs rounded-lg shadow-2xl md:max-w-sm"
-							/>
-							<div class="text-center text-neutral-content lg:text-left">
-								<h1 class="text-4xl font-bold md:text-5xl">{collection.name}</h1>
-								<p class="py-6 text-lg md:text-2xl">{collection.overview}</p>
-							</div>
+	{#if data.result}
+		{@const collection = data.result}
+		<div class="mx-auto w-full max-w-full">
+			{#await image(collection.backdrop_path, 'backdrops', true) then backdropImage}
+				<div
+					class="hero rounded-box bg-base-200"
+					style="background-image: url({backdropImage.src});"
+				>
+					<div class="hero-overlay rounded-box bg-opacity-90"></div>
+					<div class="hero-content flex-col gap-4 lg:flex-row">
+						<Img
+							alt="Poster"
+							params={[collection.poster_path, 'backdrops', true]}
+							class="max-w-xs rounded-lg shadow-2xl md:max-w-sm"
+						/>
+						<div class="text-center text-neutral-content lg:text-left">
+							<h1 class="text-4xl font-bold md:text-5xl">{collection.name}</h1>
+							<p class="py-6 text-lg md:text-2xl">{collection.overview}</p>
 						</div>
 					</div>
-				{/await}
+				</div>
+			{/await}
 
-				<div class="mt-6 w-full">
-					<div class="flex items-center justify-between">
-						<h2 class="text-3xl font-bold md:text-4xl">Filme</h2>
+			<div class="mt-6 w-full">
+				<div class="flex items-center justify-between">
+					<h2 class="text-3xl font-bold md:text-4xl">Filme</h2>
 
-						<!-- Toggle für Grid/List-Ansicht -->
-						<button class="btn btn-outline" onclick={() => (isGridView = !isGridView)}>
-							{isGridView ? 'Wechsel zu Listenansicht' : 'Wechsel zu Grid-Ansicht'}
-						</button>
-					</div>
+					<!-- Toggle für Grid/List-Ansicht -->
+					<button class="btn btn-outline" onclick={() => (isGridView = !isGridView)}>
+						{isGridView ? 'Wechsel zu Listenansicht' : 'Wechsel zu Grid-Ansicht'}
+					</button>
+				</div>
 
-					<!-- Grid-Ansicht / Listenansicht -->
-					<div
-						class="mt-4"
-						class:grid={isGridView}
-						class:grid-cols-1={isGridView}
-						class:md:grid-cols-2={isGridView}
-						class:lg:grid-cols-3={isGridView}
-						class:xl:grid-cols-4={isGridView}
-						class:2xl:grid-cols-5={isGridView}
-						class:gap-4={isGridView}
-						class:space-y-4={!isGridView}
-					>
-						{#each collection.parts as movie}
-							<a
-								href="/movie?id={movie.id}"
-								class="flex flex-col items-center gap-4 rounded-lg bg-base-200 p-4 shadow-md"
-								class:transform={isGridView}
-								class:transition-transform={isGridView}
-								class:hover:scale-105={isGridView}
-								class:md:flex-row={!isGridView}
+				<!-- Grid-Ansicht / Listenansicht -->
+				<div
+					class="mt-4"
+					class:grid={isGridView}
+					class:grid-cols-1={isGridView}
+					class:md:grid-cols-2={isGridView}
+					class:lg:grid-cols-3={isGridView}
+					class:xl:grid-cols-4={isGridView}
+					class:2xl:grid-cols-5={isGridView}
+					class:gap-4={isGridView}
+					class:space-y-4={!isGridView}
+				>
+					{#each collection.parts as movie}
+						<a
+							href="/movie?id={movie.id}"
+							class="flex flex-col items-center gap-4 rounded-lg bg-base-200 p-4 shadow-md"
+							class:transform={isGridView}
+							class:transition-transform={isGridView}
+							class:hover:scale-105={isGridView}
+							class:md:flex-row={!isGridView}
+						>
+							<Img
+								alt="Poster"
+								params={[movie.poster_path, 'posters', true]}
+								class="aspect-[2/3] w-full max-w-[15rem] rounded-lg object-cover shadow-2xl"
+							/>
+							<div
+								class:text-center={isGridView}
+								class:flex={!isGridView}
+								class:flex-col={!isGridView}
+								class:gap-2={!isGridView}
+								class:text-left={!isGridView}
 							>
-								<Img
-									alt="Poster"
-									params={[movie.poster_path, 'posters', true]}
-									class="aspect-[2/3] w-full max-w-[15rem] rounded-lg object-cover shadow-2xl"
-								/>
-								<div
-									class:text-center={isGridView}
-									class:flex={!isGridView}
-									class:flex-col={!isGridView}
-									class:gap-2={!isGridView}
-									class:text-left={!isGridView}
-								>
-									<h3 class="text-2xl font-bold">{movie.title}</h3>
-									{#if movie.title !== movie.original_title}
-										<p class="font-semibold italic">{movie.original_title}</p>
-									{/if}
-									<p>
-										{movie.release_date
-											? new Date(movie.release_date).toLocaleDateString(
-													settings ? settings.language : window.navigator.language
-												)
-											: 'Kein Datum vorhanden'}
-									</p>
-									<p>{movie.overview}</p>
-								</div>
-							</a>
-						{/each}
-					</div>
+								<h3 class="text-2xl font-bold">{movie.title}</h3>
+								{#if movie.title !== movie.original_title}
+									<p class="font-semibold italic">{movie.original_title}</p>
+								{/if}
+								<p>
+									{movie.release_date
+										? new Date(movie.release_date).toLocaleDateString(
+												settings ? settings.language : window.navigator.language
+											)
+										: 'Kein Datum vorhanden'}
+								</p>
+								<p>{movie.overview}</p>
+							</div>
+						</a>
+					{/each}
 				</div>
 			</div>
-		{:else}
-			<div class="flex h-full w-full items-center justify-center">
-				<div class="grid gap-8 text-center">
-					<p class="text-4xl md:text-5xl">Keine Daten gefunden</p>
-					<a href="/" class="btn btn-lg mt-4 text-2xl">Zurück</a>
-				</div>
+		</div>
+	{:else}
+		<div class="flex h-full w-full items-center justify-center">
+			<div class="grid gap-8 text-center">
+				<p class="text-4xl md:text-5xl">Keine Daten gefunden</p>
+				<a href="/" class="btn btn-lg mt-4 text-2xl">Zurück</a>
 			</div>
-		{/if}
-	{/await}
+		</div>
+	{/if}
 </main>
--- src/routes/collection/+page.ts
+++ src/routes/collection/+page.ts
@@ -1,18 +1,30 @@
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
+import { browser } from '$app/environment';
 
-export const load = (({ url }) => {
-	const id: number = (() => {
-		const idParam = url.searchParams.get('id');
-		if (!idParam) {
-			throw error(400, 'ID must be provided');
-		}
-		const parsedId = parseInt(idParam, 10);
-		if (isNaN(parsedId)) {
-			throw error(400, 'ID must be a valid number');
-		}
-		return parsedId;
-	})();
+function parseId(url: URL): number {
+	const idParam = url.searchParams.get('id');
+	if (!idParam) {
+		throw error(400, 'ID must be provided');
+	}
+	const parsedId = parseInt(idParam, 10);
+	if (isNaN(parsedId)) {
+		throw error(400, 'ID must be a valid number');
+	}
+	return parsedId;
+}
 
-	return { id };
+export const load = (async ({ url }) => {
+	const id = parseId(url); // ID validieren und parsen
+	let result;
+
+	if (browser) {
+		const module = await import('$lib/db/funktion');
+		result = await module.getCollection(id);
+		if (!result) {
+			throw error(404, 'Collection not found');
+		}
+	}
+
+	return { id, result };
 }) satisfies PageLoad;
