792bd77b510685a6fedde9cd4e262db48cd89ec1
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-12-19 12:38:25 +0100

    fix(error page): replace link with button for improved navigation handling

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -9,7 +9,13 @@
 				<h1 class="mb-5 text-5xl font-bold opacity-10 lg:text-7xl xl:text-9xl">Error</h1>
 				<h2 class="mb-5 text-4xl font-bold opacity-10 lg:text-6xl xl:text-8xl">{$page.status}</h2>
 				<p class="mb-5 text-3xl">{$page.error?.message}</p>
-				<a class="btn btn-outline" href="/">Zurück</a>
+				<button
+					class="btn btn-outline btn-sm md:btn-md"
+					onclick={() =>
+						window.history.length > 1 ? window.history.back() : (window.location.href = '/')}
+				>
+					{window.history.length > 1 ? 'Zurück' : 'Zur Startseite'}
+				</button>
 			</div>
 		</div>
 	</div>
