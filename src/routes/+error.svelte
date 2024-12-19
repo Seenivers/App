e3e719e63519990e2f8815feb426c34648f5a0d7
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-12-19 12:38:57 +0100

    fix(error page): update import from '$app/stores' to '$app/state' for correct state management

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 </script>
 
 <main class="flex flex-grow flex-col justify-center">
@@ -7,8 +7,8 @@
 		<div class="hero-content text-center">
 			<div class="max-w-md">
 				<h1 class="mb-5 text-5xl font-bold opacity-10 lg:text-7xl xl:text-9xl">Error</h1>
-				<h2 class="mb-5 text-4xl font-bold opacity-10 lg:text-6xl xl:text-8xl">{$page.status}</h2>
-				<p class="mb-5 text-3xl">{$page.error?.message}</p>
+				<h2 class="mb-5 text-4xl font-bold opacity-10 lg:text-6xl xl:text-8xl">{page.status}</h2>
+				<p class="mb-5 text-3xl">{page.error?.message}</p>
 				<button
 					class="btn btn-outline btn-sm md:btn-md"
 					onclick={() =>
