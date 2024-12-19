d8e55ff8920235d6f49e145cd7fa233857a90b65
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-12-19 08:15:35 +0100

    feat: add reusable Navbar component with customizable slots for left, middle, and right sections

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	interface NavbarProps {
+		left?: () => null;
+		middle?: () => null;
+		right?: () => null;
+	}
+	let { left, middle, right }: NavbarProps = $props();
+</script>
+
+<nav class="navbar sticky top-0 z-10 flex justify-between bg-base-100 p-2 shadow-lg md:p-4">
+	{#if left}
+		<div class="gap-1">
+			{@render left?.()}
+		</div>
+	{/if}
+	{#if middle}
+		<div class="gap-1">
+			{@render middle?.()}
+		</div>
+	{/if}
+	{#if right}
+		<div class="gap-1">
+			{@render right?.()}
+		</div>
+	{/if}
+</nav>
