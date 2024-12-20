3283baa3da2a2ceb8d3ade5a1bdc414068b4f44c
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-12-20 22:14:25 +0100

    feat(update): implement functions to update movies and collections, and migrate old database entries

--- src/lib/db/funktion.ts
+++ src/lib/db/funktion.ts
@@ -3,15 +3,9 @@ import { error } from '@tauri-apps/plugin-log';
 import { eq } from 'drizzle-orm';
 import { db } from '$lib/db/database';
 import { migrate } from '$lib/db/migrate';
-import { BaseDirectory, exists, readTextFile, remove } from '@tauri-apps/plugin-fs';
-import type { OldData } from '$lib/types';
-import { getMovie as getMovieTmdb, getCollection as getCollectionTmdb } from '$lib/tmdb';
+import { updateCollections, updateMovies, updateOldDB } from './update';
 
-const WEEKS = 1; // Anzahl der Wochen, nach der die Filme aktualisiert werden sollen
-const WEEK_IN_MILLIS = 6.048e8; // 1 Woche in Millisekunden
-const WEEKS_IN_MILLIS = WEEK_IN_MILLIS * WEEKS; // Dauer in Millisekunden für die gewünschte Wochen
-
-let loadedSettings: typeof schema.settings.$inferSelect | undefined;
+export let loadedSettings: typeof schema.settings.$inferSelect | undefined;
 
 async function createDefaultSettings() {
 	const language = navigator.language.substring(0, 2);
@@ -29,115 +23,6 @@ async function initializeSettings() {
 		const settings = await db.select().from(schema.settings).limit(1);
 		loadedSettings = settings[0] ?? (await createDefaultSettings());
 	}
-
-	const dataLibExists = await exists('data.lib', { baseDir: BaseDirectory.AppConfig });
-	if (dataLibExists) {
-		const content = (await readTextFile('data.lib', { baseDir: BaseDirectory.AppConfig })).trim();
-
-		if (content) {
-			const data: OldData = JSON.parse(content);
-
-			await Promise.all(
-				data.movies.map(async (movie) => {
-					const result = await getMovieTmdb(movie.id, loadedSettings?.language);
-
-					if (!result) {
-						throw new Error(`Movie with ID ${movie.id} could not be fetched.`);
-					}
-
-					await addMovie({
-						id: movie.id,
-						path: movie.path,
-						tmdb: result,
-						watched: movie.watched,
-						watchTime: movie.watchTime
-					});
-				})
-			);
-
-			await remove('data.lib', { baseDir: BaseDirectory.AppConfig });
-		}
-	}
-}
-
-async function updateEntity(
-	entity: typeof schema.movies.$inferSelect | typeof schema.collections.$inferSelect,
-	entityType: 'movies' | 'collections'
-) {
-	try {
-		const currentDate = new Date();
-
-		// Falls "updated" nicht gesetzt ist, aktualisiere es auf den aktuellen Zeitpunkt
-		if (!entity.updated) {
-			await db
-				.update(schema[entityType])
-				.set({ updated: currentDate })
-				.where(eq(schema[entityType].id, entity.id));
-		} else {
-			// Überprüfen, ob das "updated"-Datum älter als die definierte Zeitspanne ist
-			const updatedDate = new Date(entity.updated);
-			if (updatedDate.getTime() + WEEKS_IN_MILLIS < currentDate.getTime()) {
-				const result =
-					entityType === 'movies'
-						? await getMovieTmdb(entity.id, loadedSettings?.language)
-						: await getCollectionTmdb(entity.id, loadedSettings?.language);
-
-				if (result) {
-					await db
-						.update(schema[entityType])
-						.set({ ...result, updated: currentDate })
-						.where(eq(schema[entityType].id, entity.id));
-				}
-			}
-		}
-	} catch (err) {
-		error(`Fehler beim Aktualisieren der ${entityType}: ` + err);
-	}
-}
-
-async function updateMovies() {
-	try {
-		const movies = await getAllMovies();
-		if (movies && movies.length > 0) {
-			for (const movie of movies) {
-				await updateMovieIfNeeded(movie);
-			}
-		}
-	} catch (err) {
-		error('Fehler beim Aktualisieren der Filme: ' + err);
-	}
-}
-
-async function updateMovieIfNeeded(movie: typeof schema.movies.$inferSelect) {
-	await updateEntity(movie, 'movies');
-	if (movie.tmdb.belongs_to_collection) {
-		await processCollection(movie.tmdb.belongs_to_collection.id);
-	}
-}
-
-async function processCollection(collectionId: number) {
-	const collection = await getCollection(collectionId);
-	if (!collection) {
-		const result = await getCollectionTmdb(collectionId, loadedSettings?.language);
-		if (result) {
-			await addCollection({ ...result, updated: new Date() });
-		}
-	}
-}
-
-async function updateCollections() {
-	try {
-		const collections = await getAllCollections();
-		if (collections && collections.length > 0) {
-			// Collectionen nacheinander durchgehen
-			for (const collection of collections) {
-				// Aktualisiere Collection, wenn nötig
-				await updateEntity(collection, 'collections');
-			}
-		}
-	} catch (err) {
-		error('Fehler beim Aktualisieren der Collections: ' + err);
-	}
 }
 
 await initializeSettings();
@@ -151,8 +36,13 @@ if (!loadedSettings) {
  */
 export const settings = loadedSettings;
 
-updateMovies();
-updateCollections();
+(async () => {
+	if (settings && import.meta.env.PROD) {
+		await updateOldDB();
+		await updateMovies();
+		await updateCollections();
+	}
+})();
 
 export async function addMovie(data: typeof schema.movies.$inferInsert) {
 	return await db
---
+++ src/lib/db/update.ts
@@ -0,0 +1,127 @@
+import { error } from '@tauri-apps/plugin-log';
+import { eq } from 'drizzle-orm';
+import { db } from './database';
+import {
+	addCollection,
+	addMovie,
+	getAllCollections,
+	getAllMovies,
+	getCollection,
+	loadedSettings,
+	settings
+} from './funktion';
+import { BaseDirectory, exists, readTextFile, remove } from '@tauri-apps/plugin-fs';
+import type { OldData } from '$lib/types';
+import { getMovie as getMovieTmdb, getCollection as getCollectionTmdb } from '$lib/tmdb';
+import { schema } from './schema';
+
+const WEEKS = 1; // Anzahl der Wochen, nach der die Filme aktualisiert werden sollen
+const WEEK_IN_MILLIS = 6.048e8; // 1 Woche in Millisekunden
+const WEEKS_IN_MILLIS = WEEK_IN_MILLIS * WEEKS; // Dauer in Millisekunden für die gewünschte Wochen
+
+export async function updateOldDB() {
+	const dataLibExists = await exists('data.lib', { baseDir: BaseDirectory.AppConfig });
+	if (dataLibExists) {
+		const content = (await readTextFile('data.lib', { baseDir: BaseDirectory.AppConfig })).trim();
+
+		if (content) {
+			const data: OldData = JSON.parse(content);
+
+			await Promise.all(
+				data.movies.map(async (movie) => {
+					const result = await getMovieTmdb(movie.id, loadedSettings?.language);
+
+					if (!result) {
+						throw new Error(`Movie with ID ${movie.id} could not be fetched.`);
+					}
+
+					await addMovie({
+						id: movie.id,
+						path: movie.path,
+						tmdb: result,
+						watched: movie.watched,
+						watchTime: movie.watchTime
+					});
+				})
+			);
+
+			await remove('data.lib', { baseDir: BaseDirectory.AppConfig });
+		}
+	}
+}
+
+async function updateEntity(
+	entity: typeof schema.movies.$inferSelect | typeof schema.collections.$inferSelect,
+	entityType: 'movies' | 'collections'
+) {
+	try {
+		const currentDate = new Date();
+
+		// Falls "updated" nicht gesetzt ist, aktualisiere es auf den aktuellen Zeitpunkt
+		if (!entity.updated) {
+			await db
+				.update(schema[entityType])
+				.set({ updated: currentDate })
+				.where(eq(schema[entityType].id, entity.id));
+		} else {
+			// Überprüfen, ob das "updated"-Datum älter als die definierte Zeitspanne ist
+			const updatedDate = new Date(entity.updated);
+			if (updatedDate.getTime() + WEEKS_IN_MILLIS < currentDate.getTime()) {
+				const result =
+					entityType === 'movies'
+						? await getMovieTmdb(entity.id, settings?.language)
+						: await getCollectionTmdb(entity.id, settings?.language);
+
+				if (result) {
+					await db
+						.update(schema[entityType])
+						.set({ ...result, updated: currentDate })
+						.where(eq(schema[entityType].id, entity.id));
+				}
+			}
+		}
+	} catch (err) {
+		error(`Fehler beim Aktualisieren der ${entityType}: ` + err);
+	}
+}
+
+export async function updateMovies() {
+	try {
+		const movies = await getAllMovies();
+		if (movies && movies.length > 0) {
+			for (const movie of movies) {
+				await updateEntity(movie, 'movies');
+				if (movie.tmdb.belongs_to_collection) {
+					await processCollection(movie.tmdb.belongs_to_collection.id);
+				}
+			}
+		}
+	} catch (err) {
+		error('Fehler beim Aktualisieren der Filme: ' + err);
+	}
+}
+
+async function processCollection(collectionId: number) {
+	const collection = await getCollection(collectionId);
+	if (!collection) {
+		const result = await getCollectionTmdb(collectionId, settings?.language);
+		if (result) {
+			await addCollection({ ...result, updated: new Date() });
+		}
+	}
+}
+
+export async function updateCollections() {
+	try {
+		const collections = await getAllCollections();
+		if (collections && collections.length > 0) {
+			// Collectionen nacheinander durchgehen
+			for (const collection of collections) {
+				// Aktualisiere Collection, wenn nötig
+				await updateEntity(collection, 'collections');
+			}
+		}
+	} catch (err) {
+		error('Fehler beim Aktualisieren der Collections: ' + err);
+	}
+}
