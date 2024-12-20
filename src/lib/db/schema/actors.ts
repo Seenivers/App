4755b2344ea92b629d63d5dc333d8f3847efe8c3
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-12-20 22:48:09 +0100

    feat(actor schema): simplify actor schema by removing unused fields and adding TMDB reference

--- src/lib/db/schema/actors.ts
+++ src/lib/db/schema/actors.ts
@@ -1,20 +1,10 @@
+import type { Actor } from '$lib/types/actor';
 import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 // https://developer.themoviedb.org/reference/person-details
 export const actors = sqliteTable('actors', {
 	id: int('id').notNull().primaryKey().unique(),
 	name: text('name').notNull(),
-	profile_path: text('profile_path'),
-	biography: text('biography').notNull(),
-	adult: int('adult', { mode: 'boolean' }).notNull(),
-	known_for_department: text('known_for_department'),
-	birthday: int('birthday', { mode: 'timestamp' }),
-	deathday: int('deathday', { mode: 'timestamp' }),
-	also_known_as: text('also_known_as', { mode: 'json' }).notNull().$type<string[]>().default([]),
-	gender: int('gender').$type<0 | 1 | 2 | 3>().notNull().default(0),
-	homepage: text('homepage'),
-	popularity: int('popularity'),
-	imdb_id: text('imdb_id').notNull(),
-	place_of_birth: int('place_of_birth', { mode: 'timestamp' }),
+	tmdb: text('tmdb').notNull().$type<Actor>(),
 	updated: int('updated', { mode: 'timestamp' }).notNull().default(new Date(0))
 });
