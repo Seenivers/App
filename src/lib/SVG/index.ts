8a94e75efdae68448e6bcd32e04c8ef8f44195d7
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-12-29 18:52:28 +0100

    chore: remove unused SVG components from the library

--- src/lib/SVG/index.ts
+++ src/lib/SVG/index.ts
@@ -1,8 +1,1 @@
-export { default as Back } from './back.svelte';
-export { default as Before } from './before.svelte';
-export { default as Fullscreen } from './fullscreen.svelte';
-export { default as Loudness } from './loudness.svelte';
-export { default as Paused } from './paused.svelte';
-export { default as PictureInPicture } from './pictureInPicture.svelte';
-export { default as Play } from './play.svelte';
 export { NoImage } from './noImage';
