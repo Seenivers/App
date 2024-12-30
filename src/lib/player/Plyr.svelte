a1477b6b3e242110f9d8080afe62fdba357ecfff
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2024-12-30 01:26:13 +0100

    chore(player): optimize `Plyr` initialization and add `blank.mp4`
    - Added `blank.mp4` for better default handling in `Plyr.svelte`
    - Updated `Plyr` configuration to include the new blank video
    - Simplified `Plyr` options by removing redundant default settings

--- src/lib/player/Plyr.svelte
+++ src/lib/player/Plyr.svelte
@@ -7,6 +7,7 @@
 	import Plyr from 'plyr';
 	import 'plyr/dist/plyr.css';
 	import plyrSVG from '$lib/SVG/plyr.svg';
+	import blankVideo from '$lib/videos/blank.mp4';
 
 	interface Props {
 		id: number;
@@ -20,37 +21,33 @@
 
 	onMount(() => {
 		player = new Plyr(videoElement, {
-			autopause: true,
-			autoplay: false,
 			controls: [
-				'play-large',
-				'play',
-				'progress',
-				'current-time',
-				'mute',
-				'volume',
-				'captions',
-				'settings',
-				'pip',
-				'airplay',
-				'fullscreen'
+				'play-large', // The large play button in the center
+				'play', // Play/pause playback
+				'rewind', // Rewind by the seek time (default 10 seconds)
+				'fast-forward', // Fast forward by the seek time (default 10 seconds)
+				'progress', // The progress bar and scrubber for playback and buffering
+				'current-time', // The current time of playback
+				'duration', // The full duration of the media
+				'mute', // Toggle mute
+				'volume', // Volume control
+				'captions', // Toggle captions
+				'settings', // Settings menu
+				'pip', // Picture-in-picture (currently Safari only)
+				'airplay', // Airplay (currently Safari only)
+				'fullscreen' // Toggle fullscreen
 			],
-			hideControls: false,
-			clickToPlay: true,
-			disableContextMenu: true,
-			displayDuration: true,
-			iconPrefix: 'plyr',
 			invertTime: false,
 			keyboard: {
 				focused: true,
 				global: true
 			},
-			loop: { active: false },
 			seekTime: 10,
-			storage: { enabled: true, key: 'plyr' },
 			tooltips: { controls: true, seek: true },
 			loadSprite: false,
-			iconUrl: plyrSVG
+			iconUrl: plyrSVG,
+			settings: ['captions', 'quality', 'speed'],
+			blankVideo: blankVideo
 		});
 
 		player.on('ready', async () => {
@@ -97,8 +94,10 @@
 	});
 </script>
 
-<!-- svelte-ignore a11y_media_has_caption -->
-<!-- https://github.com/sveltejs/svelte/issues/5967#issuecomment-775297424 -->
-<video bind:this={videoElement} {src} {poster}>
-	The video tag does not support from you device.
-</video>
+<div class="select-none">
+	<!-- svelte-ignore a11y_media_has_caption -->
+	<!-- https://github.com/sveltejs/svelte/issues/5967#issuecomment-775297424 -->
+	<video bind:this={videoElement} {src} {poster}>
+		The video tag does not support from you device.
+	</video>
+</div>
