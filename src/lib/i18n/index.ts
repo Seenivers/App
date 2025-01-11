e29caeb5e7b2b8b9195baaf56ff10dc2afcc9be4
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-01-11 20:57:07 +0100

    Change the default localization to English and remove unused language files

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -1,9 +1,9 @@
 import { browser } from '$app/environment';
 import { init, register } from 'svelte-i18n';
 
-const defaultLocale = 'de';
+const defaultLocale = 'en';
 
-register('de', () => import('./locales/de.json'));
+register('en', () => import('./locales/en.json'));
 
 init({
 	fallbackLocale: defaultLocale,
