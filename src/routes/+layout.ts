cc94b738a82bbc8e3d9ef73426d0f5683fc5fb0e
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-01-11 12:55:28 +0100

    feat(i18n): implement internationalization with German locale support

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,3 +3,15 @@
 // See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
 export const prerender = true;
 export const ssr = false;
+
+import { browser } from '$app/environment';
+import '$lib/i18n';
+import { locale, waitLocale } from 'svelte-i18n';
+import type { LayoutLoad } from './$types';
+
+export const load: LayoutLoad = async () => {
+	if (browser) {
+		locale.set(window.navigator.language);
+	}
+	await waitLocale();
+};
