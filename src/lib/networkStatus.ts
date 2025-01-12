4c945af368921d41abd3f02032880b54dc43dd05
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-01-12 18:56:14 +0100

    feat: add network status localization in German

--- src/lib/networkStatus.ts
+++ src/lib/networkStatus.ts
@@ -1,18 +1,20 @@
 import { newToast } from '$lib/toast/toast';
 import { trace } from '@tauri-apps/plugin-log';
 import { isOnline } from './stores.svelte';
+import { _ } from 'svelte-i18n';
+import { get } from 'svelte/store';
 
 // Online-Status überwachen und Einstellungen aktualisieren
 export function networkStatus() {
 	ononline = () => {
-		const message = 'You are connected to the internet.';
+		const message = get(_)('networkStatus.online');
 		newToast('success', message);
 		trace(message);
 		isOnline.set(true);
 	};
 
 	onoffline = () => {
-		const message = 'You are not connected to the internet.';
+		const message = get(_)('networkStatus.offline');
 		newToast('error', message);
 		trace(message);
 		isOnline.set(false);
