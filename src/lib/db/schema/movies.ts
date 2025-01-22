9280124e0cd0d1e165e3fac2f07c3eaabc18dbdf
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-01-22 17:59:46 +0100

    fix(movies): make 'path' field nullable in movies schema

--- src/lib/db/schema/movies.ts
+++ src/lib/db/schema/movies.ts
@@ -3,7 +3,7 @@ import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const movies = sqliteTable('movies', {
 	id: int('id').notNull().primaryKey().unique(),
-	path: text('path').notNull().unique(),
+	path: text('path'),
 	watched: int('watched', { mode: 'boolean' }).notNull().default(false),
 	watchTime: int('watchTime').notNull().default(0),
 	tmdb: text('tmdb', { mode: 'json' }).$type<Movie>().notNull(),
