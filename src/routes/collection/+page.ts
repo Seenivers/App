a3df1761f8ae9ac9c7ceb31bd811ef37f1498522
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-01-28 18:45:02 +0100

    feat(collection): add movies array to collection load function

--- src/routes/collection/+page.ts
+++ src/routes/collection/+page.ts
@@ -2,6 +2,7 @@ import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 import { browser } from '$app/environment';
 import { parseId } from '$lib/load/loadUtils';
+import type { schema } from '$lib/db/schema';
 
 export const load = (async ({ url }) => {
 	const id = parseId(url); // ID validieren und parsen
@@ -13,17 +14,35 @@ export const load = (async ({ url }) => {
 	const module = await import('$lib/db/funktion');
 
 	let result = await module.getCollection(id);
+
+	const movies: (typeof schema.movies.$inferSelect)[] = [];
+
 	if (!result && navigator.onLine) {
 		// Daten von TMDB abrufen
 		const tmdb = await import('$lib/tmdb');
 
-		result = { ...(await tmdb.getCollection(id)), updated: new Date() };
+		const collection = await tmdb.getCollection(id);
+
+		result = { ...collection, updated: new Date() };
+
+		module.addCollection(result);
 	}
 
 	if (!result) {
 		throw error(404, 'Collection not found');
 	}
 
+	if (result.parts.length > 0) {
+		await Promise.all(
+			result.parts.map(async (part) => {
+				const movie = await module.getMovie(part.id);
+				if (movie?.path) {
+					movies.push(movie);
+				}
+			})
+		);
+	}
+
 	// Nur relevante Daten zurückgeben
-	return { id, result };
+	return { id, result, movies };
 }) satisfies PageLoad;
