480b983f1eb5a217372e5d7267f6964688f8d5f1
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-02-01 22:30:32 +0100

    fix(lib): update video format extensions to include plyr and vidstack formats

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,6 +4,8 @@ export const placeholderURL = `data:image/svg+xml;charset=utf-8,${encodeURICompo
 export const imageURL = 'https://image.tmdb.org/t/p/original';
 export const seeniversURL = 'https://seenivers.com';
 export const castImages = 5; // 5 Actors Bilder Runterladen / 0 = Alle / -1 = Keine
-export const extensions = ['mp4', 'mkv'];
+export const plyr = ['mp4', 'webm', 'ogg'];
+export const vidstack = ['mp4', 'ogg', 'ogv', 'webm', 'mov', 'm4v', 'm3u8'];
+export const extensions = [...new Set([...plyr, ...vidstack])];
 export const clearResultsOnLeave = false;
 export const WEEKS = 4; // Anzahl der Wochen, nach der die Filme aktualisiert werden sollen
