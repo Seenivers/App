ebd500b49b96e1cc44cecf62110734e0eba2aec9
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-02-13 18:34:38 +0100

    fix: update seeniversURL for development environment

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,7 +2,7 @@ import { NoImage } from '$lib/SVG';
 
 export const placeholderURL = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(NoImage)}`;
 export const imageURL = 'https://image.tmdb.org/t/p/original';
-export const seeniversURL = 'https://seenivers.com';
+export const seeniversURL = import.meta.env.DEV ? 'http://localhost:5173' : 'https://seenivers.com';
 export const plyr = ['mp4', 'webm', 'ogg'];
 export const vidstack = ['mp4', 'ogg', 'ogv', 'webm', 'mov', 'm4v', 'm3u8'];
 export const extensions = [...new Set([...plyr, ...vidstack])];
