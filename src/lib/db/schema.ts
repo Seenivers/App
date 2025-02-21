ec5b32e6f0b99818a852af94ab42865d59454cd9
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-02-21 17:26:10 +0100

    feat: add serie, season, and episode to the database schema exports

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -2,12 +2,18 @@ import { settings } from './schema/settings';
 import { movies } from './schema/movies';
 import { actors } from './schema/actors';
 import { collections } from './schema/collections';
+import { serie } from './schema/serie';
+import { episode } from './schema/episode';
+import { season } from './schema/season';
 
 export const schema = {
 	settings,
 	movies,
 	actors,
-	collections
+	collections,
+	serie,
+	season,
+	episode
 };
 
-export { settings, movies, actors, collections };
+export { settings, movies, actors, collections, serie, season, episode };
