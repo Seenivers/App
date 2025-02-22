300bd59d9bbb0d449fe5d05a8d10060deb4c8dd3
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-02-22 14:00:21 +0100

    refactor: simplify setTheme function by removing theme validation

--- src/lib/themeUtils.ts
+++ src/lib/themeUtils.ts
@@ -1,11 +1,5 @@
-import { themes } from '$lib';
 import type { Settings } from './types/settings';
 
 export function setTheme(theme: Settings['theme']) {
-	if (!themes.includes(theme as Settings['theme'])) {
-		console.warn(`Ungültiges Theme: ${theme}, Standard-Theme wird gesetzt.`);
-		theme = 'default';
-	}
-
 	document.documentElement.setAttribute('data-theme', theme);
 }
