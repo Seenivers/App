4be0fad83df6eb5c46470796ac395e8b08f81837
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-02-22 17:16:43 +0100

    feat: update movies table schema to auto-increment id and handle conflicts

--- src/lib/db/schema/movies.ts
+++ src/lib/db/schema/movies.ts
@@ -2,7 +2,7 @@ import type { Movie } from '$lib/types/movie';
 import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const movies = sqliteTable('movies', {
-	id: int('id').notNull().primaryKey().unique(),
+	id: int('id').notNull().primaryKey({ autoIncrement: true, onConflict: 'fail' }).unique(),
 	path: text('path'),
 	watched: int('watched', { mode: 'boolean' }).notNull().default(false),
 	watchTime: int('watchTime').notNull().default(0),
