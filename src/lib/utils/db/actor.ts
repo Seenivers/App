0fc59178e2aa1b842842c761e24ffd4e4f4c0831
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-03-09 11:00:49 +0100

    feat: implement actor utility functions for database operations

---
+++ src/lib/utils/db/actor.ts
@@ -0,0 +1,25 @@
+import { db } from '$lib/db/database';
+import { schema } from '$lib/db/schema';
+import { error } from '@tauri-apps/plugin-log';
+import { eq } from 'drizzle-orm';
+
+export const actor = {
+	add: async (data: typeof schema.actors.$inferInsert) =>
+		await db.insert(schema.actors).values(data),
+	get: async (id: number) => await db.query.actors.findFirst({ where: eq(schema.actors.id, id) }),
+	getAll: async () => await db.select().from(schema.actors),
+	delete: async (id: number) => await db.delete(schema.actors).where(eq(schema.actors.id, id)),
+	update: async (id: number, data: Partial<typeof schema.actors.$inferInsert>) => {
+		if (Object.keys(data).length === 0) return;
+
+		try {
+			await db.update(schema.actors).set(data).where(eq(schema.actors.id, id));
+		} catch (err) {
+			error(`Update Actor: ${err}`);
+		}
+	},
+	isIDUnique: async (id: number) => {
+		const existingActor = await actor.get(id);
+		return !existingActor;
+	}
+};
