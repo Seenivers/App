3011cf167078a7c3d060a993d31ed8c471e86237
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-03-09 10:45:47 +0100

    feat: implement collection management functions for adding, retrieving, updating, and deleting collections

---
+++ src/lib/utils/db/collection.ts
@@ -0,0 +1,27 @@
+import { db } from '$lib/db/database';
+import { schema } from '$lib/db/schema';
+import { error } from '@tauri-apps/plugin-log';
+import { eq } from 'drizzle-orm';
+
+export const collection = {
+	add: async (data: typeof schema.collections.$inferInsert) =>
+		await db.insert(schema.collections).values(data),
+	get: async (id: number) =>
+		await db.query.collections.findFirst({ where: eq(schema.collections.id, id) }),
+	getAll: async () => await db.select().from(schema.collections),
+	delete: async (id: number) =>
+		await db.delete(schema.collections).where(eq(schema.collections.id, id)),
+	update: async (id: number, data: Partial<typeof schema.collections.$inferInsert>) => {
+		if (Object.keys(data).length === 0) return;
+
+		try {
+			await db.update(schema.collections).set(data).where(eq(schema.collections.id, id));
+		} catch (err) {
+			error(`Update Collection: ${err}`);
+		}
+	},
+	isIDUnique: async (id: number) => {
+		const existingCollection = await collection.get(id);
+		return !existingCollection;
+	}
+};
