0dd72ffa17f443d076758d2413287441270b0cb8
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-03-09 11:01:17 +0100

    refactor: remove unused actor functions to streamline database interactions

--- src/lib/db/funktion.ts
+++ src/lib/db/funktion.ts
@@ -1,6 +1,4 @@
 import { schema } from '$lib/db/schema';
-import { error } from '@tauri-apps/plugin-log';
-import { eq } from 'drizzle-orm';
 import { db } from '$lib/db/database';
 import { migrate } from '$lib/db/migrate';
 import { updateActors, updateCollections, updateMovies, updateOldDB } from './update';
@@ -36,60 +34,3 @@ export const settings = await initializeSettings();
 		await updateActors();
 	}
 })();
-
-/**
- * Get Actor from db
- */
-export async function getActor(id: number) {
-	return await db.query.actors.findFirst({ where: eq(schema.actors.id, id) }).catch((err) => {
-		error('Get Actor: ' + err);
-	});
-}
-
-/**
- * Add Actor to db
- */
-export async function addActor(data: typeof schema.actors.$inferInsert) {
-	return await db
-		.insert(schema.actors)
-		.values(data)
-		.catch((err) => {
-			error('Add Actor: ' + err);
-		});
-}
-
-/**
- * Get all actors from db
- */
-export async function getAllActors(): Promise<void | (typeof schema.actors.$inferSelect)[]> {
-	return await db
-		.select()
-		.from(schema.actors)
-		.catch((err) => {
-			error('Get All Actors: ' + err);
-		});
-}
-
-export async function deleteActor(id: number) {
-	return await db
-		.delete(schema.actors)
-		.where(eq(schema.actors.id, id))
-		.catch((err) => {
-			error(`Delete Actor: ` + err);
-		});
-}
-
-export async function updateActor(id: number, data: typeof schema.actors.$inferInsert) {
-	return await db
-		.update(schema.actors)
-		.set(data)
-		.where(eq(schema.actors.id, id))
-		.catch((err) => {
-			error(`Update Actor: ` + err);
-		});
-}
-
-export async function isActorIDUnique(id: number): Promise<boolean> {
-	const existingActor = await getActor(id);
-	return !existingActor;
-}
