908698aae8e154d1b9a8431eb5c84ae091b8fba4
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-03-30 17:24:36 +0200

    feat(settings): initialize settings state from database

--- src/lib/stores.svelte.ts
+++ src/lib/stores.svelte.ts
@@ -1,3 +1,6 @@
 import type { SearchList } from '$lib/types/add';
+import { settingsDB } from './utils/db/settings';
 
 export const searchList = $state<SearchList[]>([]);
+
+export const settings = $state(await settingsDB.get());
