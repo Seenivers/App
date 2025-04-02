a00e9cdda06a282d80aeed0973c40063cad7b2b4
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-04-02 20:53:08 +0200

    feat: add card component with dynamic scaling and image support

---
+++ src/lib/utils/card.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { image } from '$lib/image/image';
+	import Img from '$lib/image/Img.svelte';
+	import type { Cardscale } from '$lib/types/cardscale';
+
+	let {
+		CARDSCALE = $bindable(),
+		href,
+		alt,
+		title,
+		params
+	}: {
+		CARDSCALE: Cardscale;
+		href: string;
+		alt: string;
+		title: string;
+		params: Parameters<typeof image>;
+	} = $props();
+</script>
+
+<a
+	{href}
+	draggable="false"
+	class="card bg-base-100 hover:bg-base-content/20 h-fit flex-grow select-none shadow-xl transition-all duration-300 hover:scale-105
+					{CARDSCALE.aktiv === 1
+		? 'min-w-[8rem] max-w-[12rem]'
+		: CARDSCALE.aktiv === 2
+			? 'min-w-[12rem] max-w-[18rem]'
+			: 'min-w-[16rem] max-w-[24rem]'}"
+>
+	<figure class="relative px-2 pt-2">
+		<Img {params} {alt} class="rounded-xl" />
+	</figure>
+	<div class="card-body items-center py-2 text-center">
+		<p
+			class="card-title {CARDSCALE.aktiv === 1
+				? 'text-base'
+				: CARDSCALE.aktiv === 2
+					? 'text-lg'
+					: 'text-2xl'}"
+		>
+			{title}
+		</p>
+	</div>
+</a>
