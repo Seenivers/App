87ecfde97c9b616d6d7ee8ca7bea6adc86f380bb
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-04-04 22:40:05 +0200

    feat: update search input styling and SVG icon for improved UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -71,19 +71,17 @@
 			<label class="input input-bordered flex w-full items-center gap-2">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5 opacity-60"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
+					viewBox="0 0 16 16"
+					fill="currentColor"
+					class="h-4 w-4 opacity-70"
 				>
 					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M21 21l-4.35-4.35M16.65 16.65A7.5 7.5 0 1116.65 2.5a7.5 7.5 0 010 14.15z"
+						fill-rule="evenodd"
+						d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+						clip-rule="evenodd"
 					/>
 				</svg>
-				<input type="text" class="w-full" placeholder="Titel suchen..." bind:value={search} />
+				<input type="text" class="grow" placeholder="Titel suchen..." bind:value={search} />
 			</label>
 		</div>
 
