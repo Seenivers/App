3bd960dd429c909f8c46aa225693e1bdaa0d4838
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-04-04 22:56:11 +0200

    feat: replace inline search SVG with reusable Search component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,7 @@
 	import { _ } from 'svelte-i18n';
 	import { type CardscaleNumbers } from '$lib/types/cardscale';
 	import { CARD_SCALE } from '$lib/utils/cardscale';
+	import Search from '$lib/SVG/search.svelte';
 
 	interface Props {
 		data: PageData;
@@ -69,18 +70,7 @@
 		<!-- Sucheingabe -->
 		<div class="mx-auto flex w-full max-w-md flex-wrap items-center gap-3 sm:max-w-lg">
 			<label class="input input-bordered flex w-full items-center gap-2">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 16 16"
-					fill="currentColor"
-					class="h-4 w-4 opacity-70"
-				>
-					<path
-						fill-rule="evenodd"
-						d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-						clip-rule="evenodd"
-					/>
-				</svg>
+				<Search class="h-4 w-4 opacity-70" />
 				<input type="text" class="grow" placeholder="Titel suchen..." bind:value={search} />
 			</label>
 		</div>
