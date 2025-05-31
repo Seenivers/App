e2a9f409bd2ba1594a572d63d8eebbea862c8bf8
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-05-31 22:10:15 +0200

    feat: enhance DaisyUI integration and update toast component styles

--- src/lib/toast/toast.svelte
+++ src/lib/toast/toast.svelte
@@ -10,11 +10,25 @@
 	const positionHorizontally: AlertPositionHorizontally =
 		settings?.toastPosition.horizontal || 'end';
 	const positionVertically: AlertPositionVertically = settings?.toastPosition.vertical || 'bottom';
+	const toastVariants = {
+		start: 'toast-start',
+		center: 'toast-center',
+		end: 'toast-end',
+		top: 'toast-top',
+		middle: 'toast-middle',
+		bottom: 'toast-bottom'
+	};
+	const toastAlertVariants = {
+		info: 'alert-info',
+		success: 'alert-success',
+		warning: 'alert-warning',
+		error: 'alert-error'
+	};
 </script>
 
 <!-- Toast Container -->
 <div
-	class={`toast z-50 toast-${positionHorizontally} toast-${positionVertically} w-fit !min-w-unset max-w-full print:hidden`}
+	class={`toast z-50 ${toastVariants[positionHorizontally]} ${toastVariants[positionVertically]} !min-w-unset w-fit max-w-full print:hidden`}
 >
 	<details class="collapse collapse-plus bg-base-100/70 backdrop-blur-sm" open>
 		<summary class="collapse-title text-xl font-medium">Info-Panel</summary>
@@ -24,7 +38,8 @@
 			{#if $messages.length > 0}
 				{#each $messages as { type, text }}
 					<div
-						class="alert alert-{type} min-h-fit w-fit flex-shrink-0 snap-center whitespace-normal break-words rounded-lg p-4 shadow-lg transition-all"
+						class="alert min-h-fit w-fit shrink-0 snap-center whitespace-normal break-words rounded-lg p-4 shadow-lg transition-all
+						{toastAlertVariants[type]}"
 						in:fly={{ duration: 400, y: -50, easing: cubicOut }}
 						out:fly={{ duration: 400, y: 100, easing: cubicIn }}
 					>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from 'tailwindcss';
-import daisyui from 'daisyui';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
@@ -13,25 +12,5 @@ export default {
 				unset: 'unset'
 			}
 		}
-	},
-
-	plugins: [daisyui],
-
-	daisyui: {
-		themes: ['light', 'dark']
-	},
-
-	safelist: [
-		'contents',
-		'toast-start',
-		'toast-center',
-		'toast-end',
-		'toast-top',
-		'toast-middle',
-		'toast-bottom',
-		'alert-info',
-		'alert-success',
-		'alert-warning',
-		'alert-error'
-	]
+	}
 } satisfies Config;
