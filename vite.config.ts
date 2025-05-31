2284984c3c0ffa9da96c7168f60e64a5c2349e7c
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-05-31 20:29:27 +0200

    refactor: update Tailwind CSS integration
    - Removed postcss.config.js as it's no longer needed.
    - Simplified app.css by importing Tailwind CSS in a single line.
    - Updated vite.config.ts to include Tailwind CSS plugin for Vite.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { vite as vidstack } from 'vidstack/plugins';
@@ -6,7 +7,7 @@ const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vidstack(), sveltekit()],
+	plugins: [tailwindcss(), vidstack(), sveltekit()],
 	build: {
 		target: 'esnext'
 	},
