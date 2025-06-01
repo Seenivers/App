83f569158fbc30d83ed244ff15e359469082bb9f
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-06-01 12:31:31 +0200

    feat: add watched field to collections schema for tracking watched status

--- src/lib/db/schema/collections.ts
+++ src/lib/db/schema/collections.ts
@@ -4,6 +4,7 @@ import type { Movie } from '$lib/types/searchMovie';
 export const collections = sqliteTable('collection', {
 	id: int('id').notNull().unique(),
 	name: text('name').notNull(),
+	watched: int('watched', { mode: 'boolean' }).notNull().default(false),
 	overview: text('overview'),
 	poster_path: text('poster_path'),
 	backdrop_path: text('backdrop_path'),
