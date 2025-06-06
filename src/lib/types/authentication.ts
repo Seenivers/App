9145fb1fd423ec6bfd7e60055057ca330f585a1a
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-06-06 17:56:04 +0200

    feat: extend the interfaces for RequestToken and AccessToken in authentication

--- src/lib/types/authentication.ts
+++ src/lib/types/authentication.ts
@@ -8,3 +8,18 @@ export interface Session {
 	success: boolean;
 	session_id: string;
 }
+
+export interface RequestToken {
+	status_message: string;
+	request_token: string;
+	success: boolean;
+	status_code: number;
+}
+
+export interface AccessToken {
+	success: boolean;
+	status_code: number;
+	status_message: string;
+	account_id: string;
+	access_token: string;
+}
