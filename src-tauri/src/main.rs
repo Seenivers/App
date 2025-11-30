14b448de17b73a646d1f6acb8ddd440302f2e81a
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-11-30 19:52:18 +0100

    Initialize Sentry in Tauri main with environment-specific configuration and error logging

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,30 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use log::LevelFilter;
+use sentry::ClientInitGuard;
+use sentry_log::SentryLogger;
+
 fn main() {
+    let _sentry: ClientInitGuard = sentry::init((
+        "https://17b503b446664415a4c43e1619aa1cdf@glitchtip.webretter.com/7",
+        sentry::ClientOptions {
+            release: Some("seenivers@1.0.0".into()), // Version deiner App
+            environment: Some(
+                if cfg!(debug_assertions) {
+                    "development"
+                } else {
+                    "production"
+                }
+                .into(),
+            ),
+            ..Default::default()
+        },
+    ));
+
+    let logger = SentryLogger::with_dest(log::logger());
+    log::set_boxed_logger(Box::new(logger)).unwrap();
+    log::set_max_level(LevelFilter::Error);
+
     seenivers_lib::run();
 }
