97b31db4cc509d21d3d8dcf6ef0cd850b41f0410
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-01-03 18:40:33 +0100

    chore(db): create index barrel for schema exports

---
+++ src/lib/db/schema/index.ts
@@ -0,0 +1,7 @@
+export * from './actors';
+export * from './collections';
+export * from './episode';
+export * from './movies';
+export * from './season';
+export * from './serie';
+export * from './settings';
