1e04cd428f00cda8991b3345748cb9b7e9c9ea1b
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-01-14 21:49:11 +0100

    refactor(stores): remove settings store in favor of settings state

--- src/lib/stores.svelte.ts
+++ src/lib/stores.svelte.ts
@@ -1,6 +1,3 @@
 import type { SearchList } from '$lib/types/add';
-import { settingsDB } from './utils/db/settings';
 
 export const searchList = $state<SearchList[]>([]);
-
-export const settings = $state(await settingsDB.get());
