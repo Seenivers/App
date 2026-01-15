705c0e089208aaa05346fa84d0ea2439c84090bd
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-01-15 13:18:01 +0100

    feat(settings): initialize settings state during client startup

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,5 +1,8 @@
 import { initSentry } from '$lib/utils/sentry';
+import { initSettings } from '$lib/utils/settings/state';
 import { handleErrorWithSentry } from '@sentry/sveltekit';
+
+await initSettings();
 
 initSentry();
 
