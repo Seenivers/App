82b4d9acf4cde10ac4a90aa0b8198358e9e532fb
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-01-18 13:03:59 +0100

    feat(layout): initialize paraglide locale in browser

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,20 @@
+import { browser } from '$app/environment';
+import { error } from '@sveltejs/kit';
+import type { LayoutLoad } from './$types';
+
 // Tauri doesn't have a Node.js server to do proper SSR
 // so we will use adapter-static to prerender the app (SSG)
 // See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
 export const prerender = true;
 export const ssr = false;
+
+export const load = (() => {
+	if (!browser) {
+		error(500, 'This operation is only supported in the browser');
+	}
+	if (!localStorage.getItem('PARAGLIDE_LOCALE')) {
+		localStorage.setItem('PARAGLIDE_LOCALE', 'en');
+	}
+
+	return {};
+}) satisfies LayoutLoad;
