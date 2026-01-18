e8e286379fefb6b7c9b216039b64f42334bd4ce9
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-01-18 12:10:07 +0100

    refactor(i18n): remove svelte-i18n layout setup

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,25 +3,3 @@
 // See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
 export const prerender = true;
 export const ssr = false;
-
-import { browser } from '$app/environment';
-import '$lib/i18n';
-import { locale, locales, waitLocale } from 'svelte-i18n';
-import type { LayoutLoad } from './$types';
-import { error } from '@sveltejs/kit';
-import { get } from 'svelte/store';
-
-export const load: LayoutLoad = async () => {
-	if (!browser) {
-		error(500, 'This operation is only supported in the browser');
-	}
-
-	const { getSettings } = await import('$lib/utils/settings/state');
-
-	const userLang = window.navigator.language.split('-')[0];
-	locale.set(get(locales).includes(getSettings().language) ? getSettings().language : userLang);
-
-	await waitLocale();
-
-	return {};
-};
