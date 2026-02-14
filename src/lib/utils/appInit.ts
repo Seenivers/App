46419d8f7bda96e166f8f29d27cbf827bfad84e0
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-02-14 19:23:40 +0100

    refactor(app): remove Discord RPC initialization

--- src/lib/utils/appInit.ts
+++ src/lib/utils/appInit.ts
@@ -1,7 +1,6 @@
 import { getCurrentWebview } from '@tauri-apps/api/webview';
 import { listen, type UnlistenFn } from '@tauri-apps/api/event';
 import { networkStatus } from '$lib/utils/networkStatus';
-import { discord } from '$lib/utils/discord';
 import { stop as stopDrpc } from 'tauri-plugin-drpc';
 import { handleElements } from '$lib/utils/utils';
 import { setTheme } from '$lib/utils/themeUtils';
@@ -30,7 +29,6 @@ export async function initApp() {
 	setTheme(getSettings().theme);
 
 	if (mainWindow) {
-		await discord();
 		startClientSession();
 	}
 
