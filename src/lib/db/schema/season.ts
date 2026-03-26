909b8a8de299e339d5e70f33434d89e00262266b
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-03-26 10:28:06 +0100

    refactor(db): use api types for season schema

--- src/lib/db/schema/season.ts
+++ src/lib/db/schema/season.ts
@@ -1,13 +1,18 @@
-import type { Season } from '$lib/types/tv/season';
+import type { api } from '$lib/trpc';
 import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+
+type ApiSeason = Awaited<ReturnType<typeof api.media.getSeasonDetails.query>>;
 
 export const season = sqliteTable('season', {
 	id: int('id').notNull().unique(),
 	path: text('path'),
 	watched: int('watched', { mode: 'boolean' }).notNull().default(false),
 	episode: int('episode').notNull().default(1),
-	tmdb: text('tmdb', { mode: 'json' }).$type<Season>().notNull(),
+	tmdb: text('tmdb', { mode: 'json' }).$type<ApiSeason>().notNull(),
 	updated: int('updated', { mode: 'timestamp' })
 		.notNull()
 		.$defaultFn(() => new Date())
 });
+
+export type Season = typeof season.$inferSelect;
+export type NewSeason = typeof season.$inferInsert;
