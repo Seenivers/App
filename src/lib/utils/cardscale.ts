d40fc04e6a7e52b5a00ca362329e0d8c30983d2f
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-03-26 10:28:14 +0100

    refactor(utils): simplify card scale types

--- src/lib/utils/cardscale.ts
+++ src/lib/utils/cardscale.ts
@@ -1,12 +1,10 @@
-import type { Cardscale } from '$lib/types/cardscale';
-
 export const scaleClasses = {
 	1: { width: 'min-w-32 max-w-48', text: 'text-base' },
 	2: { width: 'min-w-48 max-w-[18rem]', text: 'text-lg' },
 	3: { width: 'min-w-[16rem] max-w-[24rem]', text: 'text-2xl' }
 };
 
-export const CARD_SCALE: Cardscale[] = [
+export const CARD_SCALE = [
 	{ number: 1, size: 'Small' },
 	{ number: 2, size: 'Medium' },
 	{ number: 3, size: 'Large' }
