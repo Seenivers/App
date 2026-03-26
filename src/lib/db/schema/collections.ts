da94bfac687ff2dd8e8e0df208bd63f5ba90388a
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-03-26 10:28:04 +0100

    refactor(db): use api types for collections

--- src/lib/db/schema/collections.ts
+++ src/lib/db/schema/collections.ts
@@ -1,5 +1,7 @@
+import type { api } from '$lib/trpc';
 import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
-import type { Movie } from '$lib/types/searchMovie';
+
+type ApiCollection = Awaited<ReturnType<typeof api.media.getCollectionDetails.query>>;
 
 export const collections = sqliteTable('collection', {
 	id: int('id').notNull().unique(),
@@ -8,8 +10,11 @@ export const collections = sqliteTable('collection', {
 	overview: text('overview'),
 	poster_path: text('poster_path'),
 	backdrop_path: text('backdrop_path'),
-	parts: text('parts', { mode: 'json' }).notNull().$type<Movie[]>(),
+	parts: text('parts', { mode: 'json' }).notNull().$type<ApiCollection['parts']>(),
 	updated: int('updated', { mode: 'timestamp' })
 		.notNull()
 		.$defaultFn(() => new Date())
 });
+
+export type Collection = typeof collections.$inferSelect;
+export type NewCollection = typeof collections.$inferInsert;
