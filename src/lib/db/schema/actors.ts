3227cdfea78ea611b73a24236f10b0ac434b65ae
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-03-26 10:28:03 +0100

    refactor(db): use api types for actor schema

--- src/lib/db/schema/actors.ts
+++ src/lib/db/schema/actors.ts
@@ -1,12 +1,17 @@
-import type { Actor } from '$lib/types/actor';
+import type { api } from '$lib/trpc';
 import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+
+type ApiActor = Awaited<ReturnType<typeof api.media.getPersonDetails.query>>;
 
 // https://developer.themoviedb.org/reference/person-details
 export const actors = sqliteTable('actors', {
 	id: int('id').notNull().unique(),
 	name: text('name').notNull(),
-	tmdb: text('tmdb').notNull().$type<Actor>(),
+	tmdb: text('tmdb').notNull().$type<ApiActor>(),
 	updated: int('updated', { mode: 'timestamp' })
 		.notNull()
 		.$defaultFn(() => new Date())
 });
+
+export type Actor = typeof actors.$inferSelect;
+export type NewActor = typeof actors.$inferInsert;
