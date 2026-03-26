a90fc05b9edbd07553b858f98d471728f5252d26
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-03-26 10:28:07 +0100

    refactor(db): use api types for serie schema

--- src/lib/db/schema/serie.ts
+++ src/lib/db/schema/serie.ts
@@ -1,5 +1,7 @@
-import type { Serie } from '$lib/types/tv/serie';
+import type { api } from '$lib/trpc';
 import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+
+type ApiSerie = Awaited<ReturnType<typeof api.media.getTvDetails.query>>;
 
 export const serie = sqliteTable('series', {
 	id: int('id').notNull().unique(),
@@ -7,9 +9,12 @@ export const serie = sqliteTable('series', {
 	watched: int('watched', { mode: 'boolean' }).notNull().default(false),
 	wantsToWatch: int('wantsToWatch', { mode: 'boolean' }).notNull().default(false),
 	season: int('season').notNull().default(1),
-	tmdb: text('tmdb', { mode: 'json' }).$type<Serie>().notNull(),
+	tmdb: text('tmdb', { mode: 'json' }).$type<ApiSerie>().notNull(),
 	updated: int('updated', { mode: 'timestamp' })
 		.notNull()
 		.$defaultFn(() => new Date()),
 	rating: int('rating').notNull().default(0)
 });
+
+export type Serie = typeof serie.$inferSelect;
+export type NewSerie = typeof serie.$inferInsert;
