cdf413797cbea85509b5bb859e5865ea36d93cc9
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-03-26 10:28:05 +0100

    refactor(db): use api types for movies

--- src/lib/db/schema/movies.ts
+++ src/lib/db/schema/movies.ts
@@ -1,5 +1,7 @@
-import type { Movie } from '$lib/types/movie';
+import type { api } from '$lib/trpc';
 import { int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+
+type ApiMovie = Awaited<ReturnType<typeof api.media.getMovieDetails.query>>;
 
 export const movies = sqliteTable('movies', {
 	id: int('id').notNull().unique(),
@@ -7,9 +9,12 @@ export const movies = sqliteTable('movies', {
 	watched: int('watched', { mode: 'boolean' }).notNull().default(false),
 	wantsToWatch: int('wantsToWatch', { mode: 'boolean' }).notNull().default(false),
 	watchTime: int('watchTime').notNull().default(0),
-	tmdb: text('tmdb', { mode: 'json' }).$type<Movie>().notNull(),
+	tmdb: text('tmdb', { mode: 'json' }).$type<ApiMovie>().notNull(),
 	updated: int('updated', { mode: 'timestamp' })
 		.notNull()
 		.$defaultFn(() => new Date()),
 	rating: int('rating').notNull().default(0)
 });
+
+export type Movies = typeof movies.$inferSelect;
+export type NewMovies = typeof movies.$inferInsert;
