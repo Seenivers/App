f8513184c9652abe6a5e337c94f38fa0fe8b3b26
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-03-26 10:25:26 +0100

    chore(sentry): update DSN endpoint

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -3,7 +3,7 @@ import { handleErrorWithSentry } from '@sentry/sveltekit';
 
 init({
 	enabled: true,
-	dsn: 'https://4983cf9f418940b5a637ff576f3c8d4b@glitchtip.webretter.com/5',
+	dsn: 'https://92496a96fb734b209acafa4fdb59de6f@glitchtip.seenivers.com/1',
 	tracesSampleRate: 0.05, // 5 % aller Aktionen
 	replaysSessionSampleRate: 0.05, // 5 % aller Sessions
 	replaysOnErrorSampleRate: 1, // alle Fehler
