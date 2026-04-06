6a4934ce2990877f4b8c0131f3d3e3a05dffb5bd
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-04-06 21:42:14 +0200

    refactor(trpc): use client import path

--- src/lib/trpc.ts
+++ src/lib/trpc.ts
@@ -1,4 +1,4 @@
 import { seeniversURL } from '$lib';
-import { createTRPCClientInstance } from '@seenivers/api';
+import { createTRPCClientInstance } from '@seenivers/api/client';
 
 export const api = createTRPCClientInstance(`${seeniversURL}/api/trpc`);
