dd8b5c7265b4a2973fd939d0d929839dad882344
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-04-12 13:32:15 +0200

    feat(card): add pointer and context menu events

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { resolve } from '$app/paths';
 	import { image } from '$lib/image/image';
 	import Img from '$lib/image/Img.svelte';
 	import { m } from '$lib/paraglide/messages';
@@ -10,7 +11,13 @@
 		alt,
 		title,
 		watched = false,
-		params
+		params,
+		onClick,
+		onContextMenu,
+		onPointerDown,
+		onPointerUp,
+		onPointerMove,
+		onPointerCancel
 	}: {
 		CARDSCALE: 1 | 2 | 3;
 		href: string;
@@ -18,15 +25,28 @@
 		title: string;
 		watched?: boolean;
 		params: Parameters<typeof image>;
+		onClick?: (event: MouseEvent) => void;
+		onContextMenu?: (event: MouseEvent) => void;
+		onPointerDown?: (event: PointerEvent) => void;
+		onPointerUp?: (event: PointerEvent) => void;
+		onPointerMove?: (event: PointerEvent) => void;
+		onPointerCancel?: (event: PointerEvent) => void;
 	} = $props();
 </script>
 
 <a
-	{href}
+	// @ts-expect-error Zieh ist nur Filme/Serien und Sammlungen
+	href={resolve(href)}
 	draggable="false"
 	class="card bg-base-100 hover:bg-base-content/20 h-fit grow shadow-xl transition-all duration-300 select-none hover:scale-105 {scaleClasses[
 		CARDSCALE
 	].width}"
+	onclick={onClick}
+	oncontextmenu={onContextMenu}
+	onpointerdown={onPointerDown}
+	onpointerup={onPointerUp}
+	onpointermove={onPointerMove}
+	onpointercancel={onPointerCancel}
 >
 	<figure class="relative px-2 pt-2">
 		<Img {params} {alt} class="rounded-xl" />
