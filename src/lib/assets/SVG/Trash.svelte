cf6fd2051608b12527f3946bea2f6664de9714cd
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-04-12 13:32:12 +0200

    feat(icons): add Trash SVG component

---
+++ src/lib/assets/SVG/Trash.svelte
@@ -0,0 +1,14 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke-width="1.5"
+	stroke="currentColor"
+	{...$$restProps}
+>
+	<path
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		d="M6 7.5h12m-10.5 0v10.125a2.625 2.625 0 0 0 5.25 0V7.5m-5.25 0V6a1.5 1.5 0 0 1 1.5-1.5h3A1.5 1.5 0 0 1 13.5 6v1.5M4.5 7.5h15"
+	/>
+</svg>
