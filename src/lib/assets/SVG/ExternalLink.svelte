5702f31c11f095223fccf97b5e6a773e62a44f61
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-04-12 13:32:08 +0200

    feat(icons): add ExternalLink SVG component

---
+++ src/lib/assets/SVG/ExternalLink.svelte
@@ -0,0 +1,14 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke-width="1.5"
+	stroke="currentColor"
+	{...$$restProps}
+>
+	<path
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		d="M13.5 6H19m0 0v5.5M19 6 10.5 14.5M7.5 7.5h-1A2.5 2.5 0 0 0 4 10v7.5A2.5 2.5 0 0 0 6.5 20h7.5a2.5 2.5 0 0 0 2.5-2.5v-1"
+	/>
+</svg>
