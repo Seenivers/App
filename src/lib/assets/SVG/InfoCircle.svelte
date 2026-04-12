09594b4d54848be3b263714dc2f2aff3cee33621
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-04-12 13:32:10 +0200

    feat(icons): add InfoCircle SVG component

---
+++ src/lib/assets/SVG/InfoCircle.svelte
@@ -0,0 +1,14 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke-width="1.5"
+	stroke="currentColor"
+	{...$$restProps}
+>
+	<path
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		d="M11.25 11.25 11.5 16.5m.5-8.25h.008v.008H12V8.25ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+	/>
+</svg>
