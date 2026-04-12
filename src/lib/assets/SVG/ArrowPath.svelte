750abc87666b79740eda00e02016bfab0aacca53
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-04-12 13:32:03 +0200

    feat(icons): add ArrowPath SVG component

---
+++ src/lib/assets/SVG/ArrowPath.svelte
@@ -0,0 +1,14 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke-width="1.5"
+	stroke="currentColor"
+	{...$$restProps}
+>
+	<path
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		d="M16.023 9.348h4.992V4.356m-1.5 13.272a9 9 0 1 1 1.09-9.375"
+	/>
+</svg>
