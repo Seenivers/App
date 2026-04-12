e15c75d5b5c13429544541d7895e50f9599c5d87
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-04-12 13:32:09 +0200

    feat(icons): add Eye SVG component

---
+++ src/lib/assets/SVG/Eye.svelte
@@ -0,0 +1,19 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke-width="1.5"
+	stroke="currentColor"
+	{...$$restProps}
+>
+	<path
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		d="M2.036 12.322a1.012 1.012 0 0 1 0-.644C3.423 7.51 7.36 4.5 12 4.5s8.577 3.01 9.964 7.178c.07.207.07.437 0 .644C20.577 16.49 16.64 19.5 12 19.5s-8.577-3.01-9.964-7.178Z"
+	/>
+	<path
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+	/>
+</svg>
