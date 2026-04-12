0127375322f24da204982b34caf717ab39c8ef38
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2026-04-12 13:32:11 +0200

    feat(icons): add PencilSquare SVG component

---
+++ src/lib/assets/SVG/PencilSquare.svelte
@@ -0,0 +1,14 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke-width="1.5"
+	stroke="currentColor"
+	{...$$restProps}
+>
+	<path
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		d="m16.862 4.487 1.687-1.687a2.25 2.25 0 1 1 3.182 3.182L10.582 17.13a4.5 4.5 0 0 1-1.897 1.13L6 19l.74-2.685a4.5 4.5 0 0 1 1.13-1.897L16.862 4.487ZM19.5 15v3.75A2.25 2.25 0 0 1 17.25 21H5.25A2.25 2.25 0 0 1 3 18.75V6.75A2.25 2.25 0 0 1 5.25 4.5H9"
+	/>
+</svg>
